1ae35a073d9d93f9eb83f770dafe355a882dbc34
Alberto <alberto.izquierdomo@gmail.com>
2020-10-26 23:10:33 +0100

    Initial frontend interface

---
+++ AlbertoIzq-3.ExpensesDesktopApp/frontend.py
@@ -0,0 +1,76 @@
+from tkinter import *
+
+window = Tk()
+
+window.title("Expenses manager")
+
+l1 = Label(window, text = "Year")
+l1.grid(row = 0, column = 0)
+
+l2 = Label(window, text = "Month")
+l2.grid(row = 0, column = 2)
+
+l3 = Label(window, text = "Day")
+l3.grid(row = 0, column = 4)
+
+l4 = Label(window, text = "Category")
+l4.grid(row = 0, column = 6)
+
+l5 = Label(window, text = "Value [€]")
+l5.grid(row = 0, column = 8)
+
+l6 = Label(window, text = "Concept")
+l6.grid(row = 1, column = 0)
+
+
+year_text = StringVar()
+e1 = Entry(window, textvariable = year_text, width = 4)
+e1.grid(row = 0, column = 1)
+
+month_text = StringVar()
+e2 = Entry(window, textvariable = month_text, width = 2)
+e2.grid(row = 0, column = 3)
+
+day_text = StringVar()
+e3 = Entry(window, textvariable = day_text, width = 2)
+e3.grid(row = 0, column = 5)
+
+category_text = StringVar()
+e3 = Entry(window, textvariable = category_text)
+e3.grid(row = 0, column = 7)
+
+value_text = StringVar()
+e4 = Entry(window, textvariable = value_text, width = 15)
+e4.grid(row = 0, column = 9)
+
+concept_text = StringVar()
+e5 = Entry(window, textvariable = concept_text, width = 75)
+e5.grid(row = 1, column = 1, columnspan = 9)
+
+
+list1 = Listbox(window, height = 6, width = 60)
+list1.grid(row = 2, column = 0, rowspan = 6, columnspan = 8)
+
+sb1 = Scrollbar(window)
+sb1.grid(row = 2, column = 8, rowspan = 6)
+
+
+b1 = Button(window, text = "View All", width = 12)
+b1.grid(row = 2, column = 9)
+
+b2 = Button(window, text = "Search entry", width = 12)
+b2.grid(row = 3, column = 9)
+
+b3 = Button(window, text = "Add entry", width = 12)
+b3.grid(row = 4, column = 9)
+
+b4 = Button(window, text = "Update selected", width = 12)
+b4.grid(row = 5, column = 9)
+
+b5 = Button(window, text = "Delete selected", width = 12)
+b5.grid(row = 6, column = 9)
+
+b6 = Button(window, text = "Close", width = 12)
+b6.grid(row = 7, column = 9)
+
+window.mainloop()
