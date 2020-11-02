73bc7cae237b47d2ee4aea222f51a38f11b35fef
Alberto <alberto.izquierdomo@gmail.com>
2020-11-02 23:11:59 +0100

    Make the script OOP, step 2

--- AlbertoIzq-3.ExpensesDesktopApp/backend.py
+++ AlbertoIzq-3.ExpensesDesktopApp/backend.py
@@ -1,52 +1,41 @@
 import sqlite3
 
 class Database:
+    """Class to connect and manage a table in a MySQL database"""
 
     def __init__(self, db): # Constructor
-        conn = sqlite3.connect(db)
-        cur = conn.cursor()
-        cur.execute("CREATE TABLE IF NOT EXISTS expense (id INTEGER PRIMARY KEY, year integer, month integer, day integer, category text, value real, concept text)")
-        conn.commit()
-        conn.close()
+        self.conn = sqlite3.connect(db)
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS expense (id INTEGER PRIMARY KEY, year integer, month integer, day integer, category text, value real, concept text)")
+        self.conn.commit()
 
     def insert(self, year, month, day, category, value, concept):
-        if not isinstance(year, int) or not isinstance(value, float): # If year is not an int or value is not a real number, then skip inserting
-            return
-        conn = sqlite3.connect("expenses.db")
-        cur = conn.cursor()
-        cur.execute("INSERT INTO expense VALUES (NULL,?,?,?,?,?,?)", (year, month, day, category, value, concept)) # With NULL Python understands that has to increment id automatically
-        conn.commit()
-        conn.close()
+        # If year is not an int or value is not a real number, then skip inserting
+        #if not isinstance(year, int) or not isinstance(value, float): 
+        #    return
+        self.cur.execute("INSERT INTO expense VALUES (NULL,?,?,?,?,?,?)", (year, month, day, category, value, concept)) # With NULL Python understands that has to increment id automatically
+        self.conn.commit()
 
     def view(self):
-        conn = sqlite3.connect("expenses.db")
-        cur = conn.cursor()
-        cur.execute("SELECT * from expense")
-        rows = cur.fetchall()
-        conn.close()
+        self.cur.execute("SELECT * from expense")
+        rows = self.cur.fetchall()
         return rows
 
     # Or search: By one entry or by all at the same time
     def search(self, year = "", month = "", day = "", category = "", value = "", concept = ""): # With default empty strings you allow to search by only one those arguments
-        conn = sqlite3.connect("expenses.db")
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM expense WHERE year = ? OR month = ? OR day = ? OR category = ? OR value = ? OR concept = ?", (year, month, day, category, value, concept))
-        rows = cur.fetchall()
-        conn.close()
+        self.cur.execute("SELECT * FROM expense WHERE year = ? OR month = ? OR day = ? OR category = ? OR value = ? OR concept = ?", (year, month, day, category, value, concept))
+        rows = self.cur.fetchall()
         return rows
 
     def delete(self, id): # We're gonna get id from tuple and index 0
-        conn = sqlite3.connect("expenses.db")
-        cur = conn.cursor()
-        cur.execute("DELETE FROM expense WHERE id = ?", (id,))
-        conn.commit()
-        conn.close()
-
+        self.cur.execute("DELETE FROM expense WHERE id = ?", (id,))
+        self.conn.commit()
+        
     def update(self, id, year, month, day, category, value, concept):
         if not isinstance(year, int) or not isinstance(value, float): # If year is not an int or value is not a real number, then skip updating
             return
-        conn = sqlite3.connect("expenses.db")
-        cur = conn.cursor()
-        cur.execute("UPDATE expense SET year = ?, month = ?, day = ?, category = ?, value = ?, concept = ? WHERE id = ?", (year, month, day, category, value, concept, id))
-        conn.commit()
-        conn.close()
+        self.cur.execute("UPDATE expense SET year = ?, month = ?, day = ?, category = ?, value = ?, concept = ? WHERE id = ?", (year, month, day, category, value, concept, id))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
--- AlbertoIzq-3.ExpensesDesktopApp/frontend.py
+++ AlbertoIzq-3.ExpensesDesktopApp/frontend.py
@@ -18,125 +18,134 @@ CATEGORY = [
 "Other"
 ]
 
-def get_selected_row(event):
-    try:
-        global selected_tuple # You create a global variable that can be used outside of the function
-        index = list1.curselection()[0]
-        selected_tuple = list1.get(index)
+class Window(object):
+    """Class to store window object"""
 
-        # Update entry fields with selected row in listbox
-        e1.delete(0, END)
-        e1.insert(END, selected_tuple[1])
-        month_text.set(selected_tuple[2])
-        day_text.set(selected_tuple[3])
-        category_text.set(selected_tuple[4])
-        e5.delete(0, END)
-        e5.insert(END, selected_tuple[5])
-        e6.delete(0, END)
-        e6.insert(END, selected_tuple[6])
-    except IndexError:
-        pass
+    def __init__(self, window):
 
-def view_command(): # Wrapper function
-    list1.delete(0, END) # Firstly you have to delete the list
-    for row in database.view():
-        list1.insert(END, row) # Every new row is inserted at the end of the list box
+        self.window = window
+        self.window.title("Expenses manager")
 
-def search_command():
-    list1.delete(0, END)
-    for row in database.search(year_text.get(), month_text.get(), day_text.get(), category_text.get(), value_text.get(), concept_text.get()):
-        list1.insert(END, row) # Every new row is inserted at the end of the list box
+        l1 = Label(window, text = "Year")
+        l1.grid(row = 0, column = 0)
 
-def add_command():
-    database.insert(year_text.get(), month_text.get(), day_text.get(), category_text.get(), value_text.get(), concept_text.get())
-    list1.delete(0, END)
-    list1.insert(END, (year_text.get(), month_text.get(), day_text.get(), category_text.get(), value_text.get(), concept_text.get()))
-    view_command() # To see how the added entry was in fact added
+        l2 = Label(window, text = "Month")
+        l2.grid(row = 0, column = 2)
 
-def delete_command():
-    database.delete(selected_tuple[0])
-    view_command() # To see how the deleted selection was in fact deleted
+        l3 = Label(window, text = "Day")
+        l3.grid(row = 0, column = 4)
 
-def update_command():
-    database.update(selected_tuple[0], year_text.get(), month_text.get(), day_text.get(), category_text.get(), value_text.get(), concept_text.get())
-    view_command() # To see how the deleted selection was in fact deleted
+        l4 = Label(window, text = "Category")
+        l4.grid(row = 0, column = 6)
+
+        l5 = Label(window, text = "Value [€]")
+        l5.grid(row = 0, column = 8)
+
+        l6 = Label(window, text = "Concept")
+        l6.grid(row = 1, column = 0)
+
+        self.year_text = StringVar()
+        self.e1 = Entry(window, textvariable = self.year_text, width = 4)
+        self.e1.grid(row = 0, column = 1)
+
+        self.month_text = StringVar()
+        self.o1 = OptionMenu(window, self.month_text, *MONTH)
+        self.o1.configure(width = 2)
+        self.o1.grid(row = 0, column = 3)
+
+        self.day_text = StringVar()
+        self.o2 = OptionMenu(window, self.day_text, *DAY)
+        self.o2.configure(width = 2)
+        self.o2.grid(row = 0, column = 5)
+
+        self.category_text = StringVar()
+        self.o3 = OptionMenu(window, self.category_text, *CATEGORY)
+        self.o3.configure(width = 12)
+        self.o3.grid(row = 0, column = 7)
+
+        self.value_text = StringVar()
+        self.e5 = Entry(window, textvariable = self.value_text, width = 15)
+        self.e5.grid(row = 0, column = 9)
+
+        self.concept_text = StringVar()
+        self.e6 = Entry(window, textvariable = self.concept_text, width = 84)
+        self.e6.grid(row = 1, column = 1, columnspan = 9)
+
+        self.list1 = Listbox(window, height = 6, width = 60)
+        self.list1.grid(row = 2, column = 0, rowspan = 6, columnspan = 8)
+
+        sb1 = Scrollbar(window)
+        sb1.grid(row = 2, column = 8, rowspan = 6)
+ 
+        self.list1.configure(yscrollcommand=sb1.set)
+        sb1.configure(command=self.list1.yview)
+
+        self.list1.bind('<<ListboxSelect>>', self.get_selected_row)
+
+        b1 = Button(window, text = "View All", width = 12, command = self.view_command)
+        b1.grid(row = 2, column = 9)
+
+        b2 = Button(window, text = "Search entry", width = 12, command = self.search_command)
+        b2.grid(row = 3, column = 9)
+
+        b3 = Button(window, text = "Add entry", width = 12, command = self.add_command)
+        b3.grid(row = 4, column = 9)
+
+        b4 = Button(window, text = "Update selected", width = 12, command = self.update_command)
+        b4.grid(row = 5, column = 9)
+
+        b5 = Button(window, text = "Delete selected", width = 12, command = self.delete_command)
+        b5.grid(row = 6, column = 9)
+
+        b6 = Button(window, text = "Close", width = 12, command = window.destroy)
+        b6.grid(row = 7, column = 9)
+
+    def get_selected_row(self, event):
+        try:
+            index = self.list1.curselection()[0]
+            self.selected_tuple = self.list1.get(index)
+
+            # Update entry fields with selected row in listbox
+            self.e1.delete(0, END)
+            self.e1.insert(END, self.selected_tuple[1])
+            self.month_text.set(self.selected_tuple[2])
+            self.day_text.set(self.selected_tuple[3])
+            self.category_text.set(self.selected_tuple[4])
+            self.e5.delete(0, END)
+            self.e5.insert(END, self.selected_tuple[5])
+            self.e6.delete(0, END)
+            self.e6.insert(END, self.selected_tuple[6])
+        except IndexError:
+            pass
+
+    def view_command(self): # Wrapper function
+        self.list1.delete(0, END) # Firstly you have to delete the list
+        for row in database.view():
+            self.list1.insert(END, row) # Every new row is inserted at the end of the list box
+
+    def search_command(self):
+        self.list1.delete(0, END)
+        for row in database.search(self.year_text.get(), self.month_text.get(), self.day_text.get(), self.category_text.get(),
+                                   self.value_text.get(), self.concept_text.get()):
+            self.list1.insert(END, row) # Every new row is inserted at the end of the list box
+
+    def add_command(self):
+        database.insert(self.year_text.get(), self.month_text.get(), self.day_text.get(), self.category_text.get(), self.value_text.get(),
+                        self.concept_text.get())
+        self.list1.delete(0, END)
+        self.list1.insert(END, (self.year_text.get(), self.month_text.get(), self.day_text.get(), self.category_text.get(),
+                                self.value_text.get(), self.concept_text.get()))
+        self.view_command() # To see how the added entry was in fact added
+
+    def delete_command(self):
+        database.delete(self.selected_tuple[0])
+        self.view_command() # To see how the deleted entry was in fact deleted
+
+    def update_command(self):
+        database.update(self.selected_tuple[0], self.year_text.get(), self.month_text.get(), self.day_text.get(), self.category_text.get(),
+                        self.value_text.get(), self.concept_text.get())
+        self.view_command() # To see how the updated entry was in fact updated
 
 window = Tk()
-
-window.title("Expenses manager")
-
-l1 = Label(window, text = "Year")
-l1.grid(row = 0, column = 0)
-
-l2 = Label(window, text = "Month")
-l2.grid(row = 0, column = 2)
-
-l3 = Label(window, text = "Day")
-l3.grid(row = 0, column = 4)
-
-l4 = Label(window, text = "Category")
-l4.grid(row = 0, column = 6)
-
-l5 = Label(window, text = "Value [€]")
-l5.grid(row = 0, column = 8)
-
-l6 = Label(window, text = "Concept")
-l6.grid(row = 1, column = 0)
-
-
-year_text = StringVar()
-e1 = Entry(window, textvariable = year_text, width = 4)
-e1.grid(row = 0, column = 1)
-
-month_text = StringVar()
-o1 = OptionMenu(window, month_text, *MONTH)
-o1.configure(width = 2)
-o1.grid(row = 0, column = 3)
-
-day_text = StringVar()
-o2 = OptionMenu(window, day_text, *DAY)
-o2.configure(width = 2)
-o2.grid(row = 0, column = 5)
-
-category_text = StringVar()
-o3 = OptionMenu(window, category_text, *CATEGORY)
-o3.configure(width = 12)
-o3.grid(row = 0, column = 7)
-
-value_text = StringVar()
-e5 = Entry(window, textvariable = value_text, width = 15)
-e5.grid(row = 0, column = 9)
-
-concept_text = StringVar()
-e6 = Entry(window, textvariable = concept_text, width = 84)
-e6.grid(row = 1, column = 1, columnspan = 9)
-
-
-list1 = Listbox(window, height = 6, width = 60)
-list1.grid(row = 2, column = 0, rowspan = 6, columnspan = 8)
-
-sb1 = Scrollbar(window)
-sb1.grid(row = 2, column = 8, rowspan = 6)
-
-list1.bind('<<ListboxSelect>>', get_selected_row)
-
-
-b1 = Button(window, text = "View All", width = 12, command = view_command)
-b1.grid(row = 2, column = 9)
-
-b2 = Button(window, text = "Search entry", width = 12, command = search_command)
-b2.grid(row = 3, column = 9)
-
-b3 = Button(window, text = "Add entry", width = 12, command = add_command)
-b3.grid(row = 4, column = 9)
-
-b4 = Button(window, text = "Update selected", width = 12, command = update_command)
-b4.grid(row = 5, column = 9)
-
-b5 = Button(window, text = "Delete selected", width = 12, command = delete_command)
-b5.grid(row = 6, column = 9)
-
-b6 = Button(window, text = "Close", width = 12, command = window.destroy)
-b6.grid(row = 7, column = 9)
-
+Window(window)
 window.mainloop()
